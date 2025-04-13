6ba6a8bcf288b8d7f24e05fbce6d4e19e6de443b
nikorasu96 <nicolasendress@gmail.com>
2025-04-13 14:56:51 -0400

    build: limpieza de artefactos innecesarios del entorno de desarrollo

--- src/utils/db.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/db.ts
-import sql from "mssql";
-import { DB_CONFIG } from "@/config/config";
-
-export async function getConnection(): Promise<sql.ConnectionPool> {
-  return sql.connect(DB_CONFIG);
-}
-
-export default sql;
